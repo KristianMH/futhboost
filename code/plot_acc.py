487268753c540e2bfe6e528b79976d565ab1ee0f
Kristian H <krilllemh@gmail.com>
2020-11-23 23:26:22 +0100

    acc for logitraw

---
+++ code/plot_acc.py
@@ -0,0 +1,17 @@
+import numpy
+import futhark_data
+import matplotlib.pyplot as plt
+
+
+
+fp = open("auc_score_boolboost.txt", "r")
+gen = futhark_data.loads(fp.readlines()[0].strip())
+a = 0
+for x in gen:
+    a = x
+
+b = numpy.loadtxt("auc_xgboost.txt")
+plt.plot(a, label="futhboost")
+plt.plot(b, label="xgboost")
+plt.legend()
+plt.show()
