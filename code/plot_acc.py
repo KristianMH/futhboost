4608fedcfba6bbf903539a13a7761ccb283dfade
Kristian H <krilllemh@gmail.com>
2020-11-24 08:57:57 +0100

    reg acc

--- code/plot_acc.py
+++ code/plot_acc.py
@@ -15,3 +15,15 @@ plt.plot(a, label="futhboost")
 plt.plot(b, label="xgboost")
 plt.legend()
 plt.show()
+
+fp = open("reg_higgs_futh.txt", "r")
+gen = futhark_data.loads(fp.readlines()[0].strip())
+a = 0
+for x in gen:
+    a = x
+
+b = numpy.loadtxt("reg_xgboost.txt")
+plt.plot(a, label="futhboost")
+plt.plot(b, label="xgboost")
+plt.legend()
+plt.show()
