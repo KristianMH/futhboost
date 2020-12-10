29251f13392e55ab31b6204fb0f1c9e4e16ed6eb
Kristian H <krilllemh@gmail.com>
2020-12-10 15:08:00 +0100

    c api updates

--- code/c-api/c-api-demo.c
+++ code/c-api/c-api-demo.c
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <xgboost/c_api.h>
+#include <unistd.h>
 #include <sys/time.h>
 
 #define safe_xgboost(call) {                                            \
@@ -22,12 +23,17 @@ if (err != 0) {                                                         \
 int main(int argc, char** argv) {
   int silent = 0;
   int use_gpu = 1;  // set to 1 to use the GPU for training
-  struct timeval tv1, tv2;
+  struct timeval start, end;
+
+  //gettimeofday(&start, NULL);
   // load the data
   //DMatrixHandle dtrain, dtest;
   DMatrixHandle dtrain;
-  safe_xgboost(XGDMatrixCreateFromFile("../data/HIGGS_5M.csv?format=csv&label_column=0",
-                                       silent, &dtrain));
+  /* safe_xgboost(XGDMatrixCreateFromFile("../data/HIGGS_training.csv?format=csv&label_column=0", */
+  /*                                      silent, &dtrain)); */
+  //safe_xgboost(XGDMatrixCreateFromFile("test.bin", silent, &dtrain));
+  safe_xgboost(XGDMatrixCreateFromFile("5M.bin", silent, &dtrain));
+  //safe_xgboost(XGDMatrixSaveBinary(dtrain, "5M.bin", 0));
   //safe_xgboost(XGDMatrixCreateFromFile("../data/agaricus.txt.test", silent, &dtest));
 
   // create the booster
@@ -45,46 +51,44 @@ int main(int argc, char** argv) {
     // this is not necessary, but provided here as an illustration
     //safe_xgboost(XGBoosterSetParam(booster, "gpu_id", "0"));
     printf("USING GPU!\n");
-    safe_xgboost(XGBoosterSetParam(booster, "max_bin", "1024"));
+    safe_xgboost(XGBoosterSetParam(booster, "max_bin", "256"));
   } else {
     // avoid evaluating objective and metric on a GPU
     safe_xgboost(XGBoosterSetParam(booster, "gpu_id", "-1"));
   }
 
-  //safe_xgboost(XGBoosterSetParam(booster, "objective", "binary:logitraw"));
-  safe_xgboost(XGBoosterSetParam(booster, "objective", "reg:squarederror"));
+  safe_xgboost(XGBoosterSetParam(booster, "objective", "binary:logitraw"));
+  //safe_xgboost(XGBoosterSetParam(booster, "objective", "reg:squarederror"));
   //safe_xgboost(XGBoosterSetParam(booster, "min_child_weight", "1"));
   safe_xgboost(XGBoosterSetParam(booster, "eta", "0.1"));
   safe_xgboost(XGBoosterSetParam(booster, "reg_lambda", "0.5"));
   //safe_xgboost(XGBoosterSetParam(booster, "gamma", "0.0"));
   //safe_xgboost(XGBoosterSetParam(booster, "predictor", "gpu_predictor"));
-  safe_xgboost(XGBoosterSetParam(booster, "max_depth", "6"));
+  safe_xgboost(XGBoosterSetParam(booster, "max_depth", "12"));
   safe_xgboost(XGBoosterSetParam(booster, "missing", "-999.0"));
   safe_xgboost(XGBoosterSetParam(booster, "verbosity", silent ? "0" : "1"));
 
   // train and evaluate for 10 iterations
-  int n_trees = 100;
+  int n_trees = 500;
   //const char* eval_names[2] = {"train", "test"};
   const char* eval_names[1] = {"train"};
   const char* eval_result = NULL;
   int i;
-  gettimeofday(&tv1, NULL);
+  gettimeofday(&start, NULL); 
+
   for (i = 0; i < n_trees; ++i) {
     safe_xgboost(XGBoosterUpdateOneIter(booster, i, dtrain));
     safe_xgboost(XGBoosterEvalOneIter(booster, i, eval_dmats, eval_names, 1, &eval_result));
     //printf("%s\n", eval_result);
   }
-  gettimeofday(&tv2, NULL);
-  printf ("Total time elapsed: %f\n",
-	  (double) (tv2.tv_usec - tv1.tv_usec) + 
-	  (double) (tv2.tv_sec - tv1.tv_sec)*1e6);
-  safe_xgboost(XGBoosterEvalOneIter(booster, i, eval_dmats, eval_names, 1, &eval_result));
+  gettimeofday(&end, NULL);
+
+  double time_taken = (end.tv_usec - start.tv_usec) + (end.tv_sec - start.tv_sec)*1e6;
+  printf("time program took %f us to execute\n", time_taken);
+
+
+  safe_xgboost(XGBoosterEvalOneIter(booster, i-1, eval_dmats, eval_names, 1, &eval_result));
   printf("%s\n", eval_result);
-
-
-  bst_ulong num_feature = 0;
-  safe_xgboost(XGBoosterGetNumFeature(booster, &num_feature));
-  printf("num_feature: %llu\n", num_feature);
 
   // predict
   bst_ulong out_len = 0;
